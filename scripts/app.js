91a837a1b0af5e57ba3cad605515583fe7f14baa
runrun <runrun0319@163.com>
2018-07-10 18:33:25 +0800

    first day1
    dd is not defined

--- scripts/app.js
+++ scripts/app.js
@@ -7,19 +7,19 @@ yike1.run(["$rootScope",function($rootScope){
      	var dds = document.querySelectorAll(dd);
         if($rootScope.collapsed){
 	     	for(var i=0;i<dds.length;i++){
-	     	    dd[i].style.transitionDuration = (i + 1) * 0.15 + 's';
-				dd[i].style.transitionProperty = 'all';
-				dd[i].style.transitionDelay = '0.2s';
-				dd[i].style.transitionTimingFunction = 'ease-out';
-				dd[i].style.transform = 'translate(0)';
+	     	    dds[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+				// dds[i].style.transitionProperty = 'all';
+				dds[i].style.transitionDelay = '0.2s';
+				dds[i].style.transitionTimingFunction = 'ease-out';
+				dds[i].style.transform = 'translate(0)';
 	     	}
         }else{
-        	for(var i=dd.length - 1; i>=0; i--) {
-			dd[i].style.transitionDuration = (dd.length - i + 1) * 0.05 + 's';
-			dd[i].style.transitionProperty = 'all';
-			dd[i].style.transitionDelay = '';
-			dd[i].style.transitionTimingFunction = 'ease-out';
-			dd[i].style.transform = 'translate(-100%)';
+        	for(var i=dds.length - 1; i>=0; i--) {
+			dds[i].style.transitionDuration = (dds.length - i + 1) * 0.05 + 's';
+			// dds[i].style.transitionProperty = 'all';
+			dds[i].style.transitionDelay = '';
+			dds[i].style.transitionTimingFunction = 'ease-out';
+			dds[i].style.transform = 'translate(-100%)';
 		    }
         }
      }
